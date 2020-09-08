55825196756560377e617724fc83e231009524d1
taylorschmidt <taylorhillschmidt@gmail.com>
2020-09-08 17:45:41 -0600

    Year 2 - I added more elements. My power grows with my dedication and hard work

--- app.js
+++ app.js
@@ -11,3 +11,24 @@ console.log(h1)
 h1.innerText = "Hogwarts"
 container.appendChild(h1)
 
+const h2 = document.createElement('h2')
+h2.innerText = "Taylor"
+container.appendChild(h2)
+
+const h3 = document.createElement('h3')
+h3.innerText = "Ravenclaw"
+container.appendChild(h3)
+
+const petType = document.createElement('h4')
+petType.className = "gecko"
+const myPet = document.createElement('h4')
+myPet.innerText = "Poe"
+container.appendChild(petType)
+petType.appendChild(myPet)
+
+const myWand = document.createElement('h4')
+myWand.innerText = "Holly Wand with Unicorn Hair Core"
+container.appendChild(myWand)
+
+
+
