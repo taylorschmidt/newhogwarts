b52c64f1e6888d97917b460ef19dab133409fe81
taylorschmidt <taylorhillschmidt@gmail.com>
2020-09-08 17:16:56 -0600

    Year 1 - added h1 using javascript

--- app.js
+++ app.js
@@ -2,3 +2,12 @@
 //     console.log('DOM is loaded!')
 // })
 
+const container = document.querySelector("#container");
+console.log(container)
+
+const h1 = document.createElement('h1')
+console.log(h1)
+
+h1.innerText = "Hogwarts"
+container.appendChild(h1)
+
