09685f953574f15f5bba8878214734e6c976e3ac
taylorschmidt <taylorhillschmidt@gmail.com>
2020-09-08 18:20:17 -0600

    Year 3 - I added list elements. That code didn't seem very dry. I wonder if there is something I could do about it in the Hungry for More section

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 //     console.log('DOM is loaded!')
 // })
 
+//year1
 const container = document.querySelector("#container");
 console.log(container)
 
@@ -11,6 +12,7 @@ console.log(h1)
 h1.innerText = "Hogwarts"
 container.appendChild(h1)
 
+//year 2
 const h2 = document.createElement('h2')
 h2.innerText = "Taylor"
 container.appendChild(h2)
@@ -29,6 +31,38 @@ petType.appendChild(myPet)
 const myWand = document.createElement('h4')
 myWand.innerText = "Holly Wand with Unicorn Hair Core"
 container.appendChild(myWand)
+
+//year 3
+const storage = document.createElement('ul')
+container.appendChild(storage)
+
+const beer = document.createElement('li')
+beer.innerText = "butter beer"
+storage.appendChild(beer)
+
+const cloak = document.createElement('li')
+cloak.className = "secret"
+cloak.innerText = "invisibility cloak"
+storage.appendChild(cloak)
+
+const map = document.createElement('li')
+map.className = 'secret'
+map.innerText = 'magic map'
+storage.appendChild(map)
+
+const turner = document.createElement('li')
+turner.className = 'secret'
+turner.innerText = 'time turner'
+storage.appendChild(turner)
+
+const leash = document.createElement('li')
+leash.className = 'gecko'
+leash.innerText = 'leash'
+storage.appendChild(leash)
+
+const beans = document.createElement('li')
+beans.innerText = "Bertie Bott's Every Flavor [Jelly] Beans"
+storage.appendChild(beans)
 
 
 
